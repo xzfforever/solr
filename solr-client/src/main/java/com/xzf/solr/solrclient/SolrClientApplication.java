d94808d7f91f8ebcb77a00b8743094284d6aed1e
xzf <270035679@qq.com>
2017-06-30 18:00:43 +0800

    add project

---
+++ solr-client/src/main/java/com/xzf/solr/solrclient/SolrClientApplication.java
@@ -0,0 +1,12 @@
+package com.xzf.solr.solrclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrClientApplication.class, args);
+	}
+}
